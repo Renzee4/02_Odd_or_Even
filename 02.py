1606897e7edc2004f04f8c28e9c802f85585dab0
Renzee4 <srenzee@gmail.com>
2016-02-20 15:27:03 -0800

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,13 +1,31 @@
 #
-
+#User input numbers
 num = input('Enter a number: ')
 num = int(num)
 
-def number(num):
-	if num%2 == 0:
-		return "This number is an even number, hooray"
-	else:
-		return "This number is an odd number for an odd person. LIKE YOU"
+check = input('Enter another number: ')
+check = int(check)
 
-print number(num)
+print " "
 
+#divisible by 2
+if num%2 == 0:
+	print "This number is an even number, hooray"
+else:
+	print "This number is an odd number for an odd person. LIKE YOU"
+
+#divisible by 4
+if num%4 == 0:
+	print "This number is a multiple of 4"
+else:
+	print "Not a multiple of 4"
+
+#users' input of two numbers divided EVENLY
+if num%check == 0:
+	print "This number divided EVENLY, you graduated prematurely"
+else: 
+	print "Nope, didn't divide evenly, sorry Felicia"
+
+
+print " "
+print "---The End---"
