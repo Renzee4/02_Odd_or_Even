603ccf33bad31e490ff16e65bd5ec1d10c48b695
Renzee4 <srenzee@gmail.com>
2016-02-20 15:08:15 -0800

    Added files via upload

---
+++ 01.py
@@ -0,0 +1,17 @@
+name = raw_input('Give me your name: ')
+
+print ('Hi ' + name + '!')
+
+age = input('Enter your age: ')
+age = int(age)
+
+print "your age is:", age
+
+current_year = input('What year is it?: ')
+current_year = int(current_year)
+
+years_till_100 = 100 - age
+Year_100 = years_till_100 + current_year
+
+print "You will be 100 years old in year: ", 
+
