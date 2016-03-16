1d7d9eddf7f638c2b1990a451cfc5e740065c661
Renzee4 <srenzee@gmail.com>
2016-03-16 12:14:20 -0700

    Added files via upload

---
+++ 11.py
@@ -0,0 +1,10 @@
+num = int(raw_input('Pick a number Fren :] '))
+
+def isItPrime(num):
+	if num%2 == 1:
+		print 'hey fren, this is a prime number'
+	else:
+		print 'hey fren, this is an even number'
+
+isItPrime(num)
+
